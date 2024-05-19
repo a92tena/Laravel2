1396e600e35e5a71cfd85e7da3cbbed1bf177af0
a92tena <albertotena_92@hotmail.com>
2024-05-19 13:35:04 +0200

    Solucion de Problemas

--- Proyecto_Laravel/resources/views/Proyectos/about.blade.php
+++ Proyecto_Laravel/resources/views/Proyectos/about.blade.php
@@ -1,4 +1,8 @@
 <x-layouts.layout>
-<h1> Estamas en la pagina del about del proyecto</h1>
-<h2> asdadadad</h2>
+<h1> Estamas en la pagina del about </h1>
+<h2> Que hemos hecho</h2>
+    <p>Hemos creado una pagina con un freamwork laravel en donde hemos aprendido a: </p>
+    <h4> Optimizar el trabajo creando layouts y asi que todas las paginas utilizen el mismo</h4>
+    <h4> Crear una base de datos y contactar con ella</h4>
+    <h4> Hacer un curl donde hemos aprendido a gestionar una clase en este caso alumno y trabajar con todas las funcionalidades que nos ofrece el uso de laravel</h4>
 </x-layouts.layout>
